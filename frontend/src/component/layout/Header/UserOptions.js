bee1420555940c51180c056690eff014b820b560
Anish <87910771+AnishLohiya@users.noreply.github.com>
2023-07-04 04:32:36 +0530

    more changes

--- frontend/src/component/layout/Header/UserOptions.js
+++ frontend/src/component/layout/Header/UserOptions.js
@@ -70,7 +70,7 @@ const UserOptions = ({ user }) => {
                 icon={
                     <img
                         className="speedDialIcon"
-                        src={user.avatar?.url ?? "/Profile.png"}
+                        src={user && user.avatar && user.avatar.url ? user.avatar.url : "/Profile.png"}
                         alt="Profile"
                     />
                 }
