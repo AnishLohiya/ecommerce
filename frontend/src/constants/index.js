2f990b69933e52d14182304e135c45b8ca3307f5
Anish <87910771+AnishLohiya@users.noreply.github.com>
2023-07-08 03:44:47 +0530

    token options update

--- frontend/src/constants/index.js
+++ frontend/src/constants/index.js
@@ -3,6 +3,6 @@ import axios from "axios";
 export const BACKEND = 
     process.env.NODE_ENV === "production"
         ? "https://mern-backend-dav5.onrender.com"
-        : "http://localhost:5000";
+        : "http://localhost:4000";
 
 export const api = axios.create({ withCredentials: true });
