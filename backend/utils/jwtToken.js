094f6f5912e0026fe135bf155b42c3fe6b1fdc4a
Anish <87910771+AnishLohiya@users.noreply.github.com>
2023-07-08 03:53:35 +0530

    token options update

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -5,7 +5,7 @@ const sendToken = (user, statusCode, res) => {
         expires: new Date(Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000), 
         httpOnly: true,
         sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
-        secure: process.env.NODE_ENV === "production" ? "true" : "false",
+        secure: process.env.NODE_ENV === "production" ? true : false,
     }
 
     res.status(statusCode).cookie("token", token, options).json({
