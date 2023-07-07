e1b2840bee98de93a36129fcd5d14bc2ca10ad52
Anish <87910771+AnishLohiya@users.noreply.github.com>
2023-07-08 03:22:21 +0530

    url fixed

--- frontend/src/constants/index.js
+++ frontend/src/constants/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const BACKEND = 
     process.env.NODE_ENV === "production"
-        ? "https://mern-backend-dav5.onrender.com/"
+        ? "https://mern-backend-dav5.onrender.com"
         : "http://localhost:5000";
 
 export const api = axios.create({ withCredentials: true });
